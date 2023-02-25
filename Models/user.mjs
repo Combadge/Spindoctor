367157b40cc8f5fe7b132c7cc53b3b2242735fc4
mo-g <mo-g@users.noreply.github.com>
2023-02-25 16:58:20 +0000

    Added different name formats.

--- Models/user.mjs
+++ Models/user.mjs
@@ -35,9 +35,9 @@
  *   sense when used by other people.
  */
 
- userTemplate = { 
+userTemplate = { 
     userName: "u-fallbackuser", // Globally unique ID
-    fullName: new UserFullName(foreName = "Fallback", familyName = "User", nickName = "Fallback User"),
+    fullName: new UserFullName(personalName = "Fallback", familyName = "User", nickName = "Fallback User"),
     ipaPronunciation: "fɔːlbæk ˈjuːzə", // Not currently in use for STT or TTS but worth adding.
     groups: [],
 
@@ -56,34 +56,132 @@ groupTemplate = {
 
 
 /**
- * Frst pass. Probably want to restructure this to allow returning different form of name.
+ * First pass. Probably want to restructure this to allow returning different form of name.
+ * Also we'll usually be left with a trailing space right now, which needs fixing.
  * 
- * Assume middle names to include patronyms. Should obviously be treated as an ordered list regardless.
  * Do we want to merge pronunciation in to this as well?
+ * 
+ * Geronymic is intended as a gender neutral word for the Slavic Patronymic,
+ * and any matriarchal anternative hat does or does not exist. Invented to my knowledge by
+ * Stephanie Lackas Soressi on Stackexchange. Thanks Steph!
+ * 
+ * A mononym is encoded as a personal name, for any avoidance of doubt. Writing a naming
+ * library for a Trek-derived software stack without supporting the names "Spock" or "Sarek"
+ * would be most tactless.
  */
 class UserFullName {
-    constructor ({prefix = "", foreName = "", middleNames = [], familyName = "", suffix = "", nickName = ""} = {}) {
+    constructor ({prefix = "",
+                  personalName = "",
+                  middleNames = [],
+                  familyName = "",
+                  suffix = "",
+                  nickName = "",
+                  geronymic = ""} = {}) {
         this.prefix = prefix;
-        this.foreName = foreName;
+        this.personalName = personalName;
         this.middleNames = middleNames;
         this.familyName = familyName;
         this.suffix = suffix;
         this.nickName = nickName;
+        this.geronymic = geronymic;
 
-        preferredFormat = this.fullName;
+        preferredFormat = this.westernName;
     };
 
     fullName () {
         var assembled = "";
-        if (this.prefix) {assembled += this.prefix};
-        if (this.foreName) {assembled += this.foreName};
+        if (this.prefix) {assembled += this.prefix + " "};
+        if (this.personalName) {assembled += this.personalName + " "};
         if (this.middleNames) {
             this.middleNames.forEach(function (name) {
-                assembled += name;
+                assembled += name + " ";
             });
         };
-        if (this.familyName) {assembled += this.familyName};
+        if (this.familyName) {assembled += this.familyName + " "};
         if (this.suffix) {assembled += this.suffix};
+
+        if (assembled) {
+            return assembled;
+        } else {
+            throw "Cannot stringify empty name."
+        };
+    };
+
+    /**
+     * The science officer formerly known as...
+     */
+    mononym () {
+        var assembled = "";
+        if (this.prefix) {assembled += this.prefix + " "};
+        if (this.personalName) {assembled += this.personalName};
+
+        if (assembled) {
+            return assembled;
+        } else {
+            throw "Cannot stringify empty name."
+        };
+    };
+
+    /**
+     * First past of an anglosphere "short name"
+     */
+    westernName () {
+        var assembled = "";
+        if (this.prefix) {assembled += this.prefix + " "};
+        if (this.personalName) {assembled += this.personalName + " "};
+        if (this.familyName) {assembled += this.familyName};
+
+        if (assembled) {
+            return assembled;
+        } else {
+            throw "Cannot stringify empty name."
+        };
+    };
+
+    /**
+     * If someone can provide a better name for this name structure, please do. Also,
+     * if I've made any error in handling titles - please let me know.
+     */
+    slavicName () {
+        var assembled = "";
+        if (this.prefix) {assembled += this.prefix + " "};
+        if (this.personalName) {assembled += this.personalName + " "};
+        if (this.geronymic) {assembled += this.geronymic + " "};
+        if (this.familyName) {assembled += this.familyName};
+
+        if (assembled) {
+            return assembled;
+        } else {
+            throw "Cannot stringify empty name."
+        };
+    };
+
+    /**
+     * I really want to implement this in a way that handles titles properly, but I'm not
+     * confident of getting that right on the first pass, hence I've gone basic and just
+     * stuck with "family fore" for now, so we at least show the intent of properly handling
+     * names from different cultures than say - the anglosphere.
+     */
+    japaneseName () {
+        var assembled = "";
+        if (this.familyName) {assembled += this.familyName + " "};
+        if (this.personalName) {assembled += this.personalName};
+
+        if (assembled) {
+            return assembled;
+        } else {
+            throw "Cannot stringify empty name."
+        };
+    };
+
+    /**
+     * This is fairly rudimentary, but it'll do for now.
+     */
+    tlhInganName () {
+        var assembled = "";
+        if (this.personalName) {assembled += this.personalName + " "};
+        assembled += "child of "
+        if (this.geronymic) {assembled += this.geronymic};
 
         if (assembled) {
             return assembled;
